ac4ef19af026a2c661f9dcb1daf1618c7cedea45
Super User <shiyi_zh0408@163.com>
2025-05-09 11:56:52 +0800

    Update inference script

--- scripts/inference/Inference.sh
+++ scripts/inference/Inference.sh
@@ -1,7 +1,11 @@
 export NCCL_P2P_LEVEL=NVL
 export NCCL_TIMEOUT=7200
 export HCCL_CONNECT_TIMEOUT=7200
-export MODEL_PATH="/Your/CogVideoX-5b-I2V/Path" # Please set your CogVideoX-5b-I2V path
+
+# Please set your CogVideoX-5b-I2V path
+export MODEL_PATH="/Your/CogVideoX-5b-I2V/Path" 
+# Set your input image path. This is an example from https://huggingface.co/datasets/shiyi0408/FlexiAct
+export INPUT_IMAGE="benchmark/target_images/human/game_girl_1.webp" 
 
 which python
 
@@ -10,7 +14,7 @@ CUDA_VISIBLE_DEVICES=0 python scripts/inference.py \
     --refadapter_ckpt_path ckpts/refnetlora_step40000_model.pt \
     --emb_ckpt_path ckpts/FAE/motion_ckpts/rotate.pt \
     --output_path outputs \
-    --gt_img_path benchmark/target_images/human/game_girl_1.webp \
+    --gt_img_path $INPUT_IMAGE \
     --prompt "A woman is performing a fitness exercise. She clasps her hands together, placing them in front of her chest, and keeps her forearms horizontal. She stands with her legs apart. She rotates her upper body, and her arms rotate along with her upper body. Meanwhile, her hands remain clasped together, and her lower body stays stationary." \
     --reweight_scale 1.0
     
